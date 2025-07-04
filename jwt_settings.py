c77a1cee35aedc2b80025e1f4331687feb68fba9
kaicretushka <ohaeohae673@gmail.com>
2025-07-05 00:01:50 +0300

    cookie name

--- jwt_settings.py
+++ jwt_settings.py
@@ -3,7 +3,7 @@ from datetime import timedelta
 
 config = AuthXConfig()
 config.JWT_SECRET_KEY="secret"
-config.JWT_ACCESS_CSRF_COOKIE_NAME="store_token"
+config.JWT_ACCESS_COOKIE_NAME="store_token"
 config.JWT_TOKEN_LOCATION=["cookies"]
 config.JWT_ACCESS_TOKEN_EXPIRES = timedelta(days=1)
 config.JWT_COOKIE_CSRF_PROTECT = False
